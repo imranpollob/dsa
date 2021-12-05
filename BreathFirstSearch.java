648b5fa069dfa006a8da563cb6a35b63abdaaf5a
pollmix <polboy777@gmail.com>
2021-12-05 00:20:59 -0500

    auto format

--- BreathFirstSearch.java
+++ BreathFirstSearch.java
@@ -9,7 +9,7 @@ public class BreathFirstSearch {
 		list.get(u).add(v);
 		list.get(v).add(u);
 	}
-	
+
 	public static void bfs(ArrayList<ArrayList<Integer>> list, int no_of_v, int source) {
 		Queue<Integer> queue = new LinkedList<Integer>();
 		// lower boolean has two values "true" and "false"
@@ -17,30 +17,28 @@ public class BreathFirstSearch {
 		// if initialized with boolean then array will be filled with false
 		// if initialized with Boolean then, filled with null
 		boolean[] visited = new boolean[no_of_v];
-		
+
 		queue.add(source);
 		visited[source] = true;
 
-		while(!queue.isEmpty()) {
+		while (!queue.isEmpty()) {
 			int u = queue.poll();
 			System.out.println(u);
-			
-			for(int v : list.get(u)) {
-				if(!visited[v]) {					
+
+			for (int v : list.get(u)) {
+				if (!visited[v]) {
 					queue.add(v);
 					visited[v] = true;
 				}
-				
 			}
 		}
-		
 	}
 
 	public static void main(String[] args) {
 		int no_of_v = 5;
 		ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>(no_of_v);
-		
-		for(int i=0; i<no_of_v; i++) {
+
+		for (int i = 0; i < no_of_v; i++) {
 			list.add(new ArrayList<Integer>());
 		}
 		// https://ibb.co/0qc9zFw
@@ -51,9 +49,9 @@ public class BreathFirstSearch {
 		addEdge(list, 2, 3);
 		addEdge(list, 2, 4);
 		addEdge(list, 3, 4);
-		
+
 		System.out.println(list);
-		
+
 		bfs(list, no_of_v, 1);
 	}
 
